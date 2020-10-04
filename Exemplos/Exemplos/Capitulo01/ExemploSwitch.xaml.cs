e58248cdda33e26158e9ad07cb840da52cb1db6a
Ione Souza Junior <junior@ionixjunior.com.br>
2020-10-04 11:11:38 -0300

    Criar telas dos primeiros exemplos do capítulo 1

---
+++ Exemplos/Exemplos/Capitulo01/ExemploSwitch.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Exemplos.Capitulo01
+{
+    public partial class ExemploSwitch : ContentPage
+    {
+        public ExemploSwitch()
+        {
+            InitializeComponent();
+        }
+    }
+}
