03fcd711d7c5f48a2a7859586617028ca0267cd7
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-10-25 21:42:38 -0300

    adicionado exemplo do Stepper

---
+++ Exemplos/Exemplos/Capitulo01/ExemploStepper.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exemplos.Capitulo01
+{
+	public partial class ExemploStepper : ContentPage
+	{
+		public ExemploStepper()
+		{
+			InitializeComponent();
+		}
+
+		private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			Console.WriteLine($"O novo valor é: {e.NewValue}");
+		}
+	}
+}
