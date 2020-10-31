c03400fc1fb6c26bef569d8c6f62f1d97a2d6a7b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-10-31 20:44:02 -0300

    adicionado exemplo de search bar (#9)

---
+++ Exemplos/Exemplos/Capitulo01/ExemploSearchBar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exemplos.Capitulo01
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ExemploSearchBar : ContentPage
+	{
+		public ExemploSearchBar()
+		{
+			InitializeComponent();
+		}
+
+		void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
