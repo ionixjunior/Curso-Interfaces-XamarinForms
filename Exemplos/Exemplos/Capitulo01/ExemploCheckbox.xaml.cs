47ac3186e3dced0dfc93022d86013b283cb5ecb4
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-01-24 21:49:14 -0300

    Adicionado Checkbox e RadioButton (#14)

---
+++ Exemplos/Exemplos/Capitulo01/ExemploCheckbox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exemplos.Capitulo01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExemploCheckbox : ContentPage
+    {
+        public ExemploCheckbox()
+        {
+            InitializeComponent();
+        }
+
+        void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Exemplos/Exemplos/Capitulo01/ExemploRadioButton.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exemplos.Capitulo01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExemploRadioButton : ContentPage
+    {
+        public ExemploRadioButton()
+        {
+            InitializeComponent();
+        }
+
+        void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+    }
+}
